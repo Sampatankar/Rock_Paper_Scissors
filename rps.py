101160a88c05043bd45090ea0f2f8a925a80202d
Sam Patankar <sampatankar@gmail.com>
2023-09-19 21:31:27 +0100

    Refactored first choices in game

--- rps.py
+++ rps.py
@@ -16,7 +16,12 @@ Logic:
 - Ask for the players name, and ask them if they wish to play, which starts a loop if they say yes to play
 - In the loop:
     - the computer asks the player to choose one of rock, paper or scissors
-    - 
+
+Spec revisit - 19/9/23:
+* Narrator is computer:
+1. Gather name of the human player
+2. Choose who goes first - human or computer
+3. One function for playing - discrete function or two lines of logic dependent on who wins first choice?
 """
 
 #Imports:
@@ -28,6 +33,14 @@ counter_computer = 0
 counter_player = 0
 count = 0
 
+choices = ["rock", "paper", "scissors"]
+
+win_loss = {
+    "rock": ["paper", "scissors"],
+    "paper": ["scissors", "rock"],
+    "scissors": ["rock", "paper"] 
+}
+
 
 #Computer asks for Players name and asks if they'd like to play Rock, Paper, Scissors:
 player = input("What is your name please? ")
@@ -38,7 +51,7 @@ go_play = input(f"Hi {player}, would you like to play today? (yes/no) ").lower()
 if go_play == "no":
     print("Wuss! Seesya and have a good day!")
 
-#On 'yes', lets play:
+#On 'yes', lets play & first go:
 elif go_play == "yes":
     #Build tension in pre-game: (Code needs to be more Pythonic!)
     print("Let's go play! Please wait while we randomly choose first play!")
@@ -49,13 +62,27 @@ elif go_play == "yes":
     time.sleep(1)
     print("...")
 
+
     #Random function to choose player:
     player_choice = random.randint(0,1)
+
+    # Refactor computer choice to randomly choose from dictionary keys:
+    computer_first_choice = choices[random.randint(0,2)]
+
+    # Computer plays first:
     if player_choice == 0:
-        print("Computer gets first play")
+        print("Computer won the toss and gets first play!")
+        human_first_choice = input("what is your choice? Rock, paper or scissors? ").lower()
+
+    # Human plays first:
     elif player_choice == 1:
         print(f"Congratulations {player}! You have won the toss!")
+        human_first_choice = input("what is your choice? Rock, paper or scissors? ").lower()
 
 
-
+# Choice comparison and game winner decided:
+if player_choice == 0:
+    for key in win_loss:
+        print(key)
+    # Do we need to loop through? Can't we just match key value, then see if human choice matches value at index 0.
 
